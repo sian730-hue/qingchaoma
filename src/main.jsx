0b872b9ee88340114a076e7e4764fb1be7451778
Qingchaoma <sian730@gmail.com>
2026-04-29 13:34:38 +0800

    admin branch

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Admin from './qingchaoma-admin.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Admin />
   </React.StrictMode>,
 )
